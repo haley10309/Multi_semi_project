6c8b47217aef9a029a9e872686a40c5e768de56d
hayung <yoohayung10309@gmail.com>
2024-02-10 17:58:05 +0900

    2.10(토) axios로 서버 불러오기

--- src/routes/boardList.js
+++ src/routes/boardList.js
@@ -1,12 +1,25 @@
 /* BoardList.js */
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const BoardList = () => {
-  return (
-    <div>
-      게시판 목록 출력
-    </div>
-  );
-};
+    const [boardList, setBoardList] = useState([]);
+  
+    const getBoardList = async () => {
+      const resp = await (await axios.get('//localhost:8080/board')).data; // 2) 게시글 목록 데이터에 할당  
+      setBoardList(resp.data); // 3) boardList 변수에 할당
+      console.log(boardList);
+    }
+  
+    useEffect(() => {
+      getBoardList(); // 1) 게시글 목록 조회 함수 호출
+    }, []);
+  
+    return (
+      <div>
+        게시판 목록 출력
+      </div>
+    );
+  };
 
 export default BoardList;
