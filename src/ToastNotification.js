cabc53a141509f012cf0684acb80fc55ecaec3d7
hayung <yoohayung10309@gmail.com>
2024-03-11 14:55:46 +0900

    ToastNotification.js refixed

--- src/ToastNotification.js
+++ src/ToastNotification.js
@@ -1,27 +1,23 @@
-import React, { useState, useEffect } from "react";
-
-// css
+import React, { useEffect } from "react";
 import "./Toast.scss";
 
-function ToastNotification() {
-    useEffect((props) => {
-        let timer = setTimeout(() => {
-            props.setToastState(false);		// 2초 뒤, toastState가 false가 되면서 알림창이 사라진다
-        }, 2000);
+function ToastNotification({ setToastState }) {
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setToastState(false); // Update toastState after 2 seconds
+    }, 2000);
 
-        return () => { clearTimeout(timer) }
-    }, []);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [setToastState]);
 
-
-    return (
-        <div className="toast-alert">
-            <img alt="" src="img/alert.png" />
-            <p>로그인 실패했습니다 
-                <br/>
-                다시 한 번 시도해 주세요
-            </p>
-        </div>
-    );
+  return (
+    <div className="toast-alert">
+      <img alt="" src="img/alert.png" />
+      <p>로그인 실패</p>
+    </div>
+  );
 }
 
-export { ToastNotification }
+export { ToastNotification };
