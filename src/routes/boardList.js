acce5f8fe123fcafb69de202d135cb3beead971f
hayung <yoohayung10309@gmail.com>
2024-03-15 13:21:47 +0900

    boardList.js handleEdit

--- src/routes/boardList.js
+++ src/routes/boardList.js
@@ -21,44 +21,43 @@ const BoardList = () => {
 
   const currentUser = localStorage.getItem("LoginID");
 
-
   // 민경 - 리뷰 데이터를 가져오는 함수
   const fetchData = async () => {
     const params_mv = { movie_id: movieNumber };
-      try {
-        //영화 정보 가져오는 request
-        const response = await axios.get(`/myapp/movie`, { params: params_mv }); //영화 정보 가져오기
-        console.log(response.data);
-        //리뷰 정보 가져오는 request
-        const response_rv = await axios.get(`/myapp/review`, {
-          params: {
-            movie_id: movieNumber, //해당 영화 id
-            likeuseraccount: currentUser, //현재 접속한 사용자의 id -> 리뷰들에 대한 좋아요를 눌렀는지 확인
-          },
-        });
-        //리뷰 정보 저장
-        setMovies(response.data);
-        setReviews(response_rv.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
+    try {
+      //영화 정보 가져오는 request
+      const response = await axios.get(`/myapp/movie`, { params: params_mv }); //영화 정보 가져오기
+      console.log(response.data);
+      //리뷰 정보 가져오는 request
+      const response_rv = await axios.get(`/myapp/review`, {
+        params: {
+          movie_id: movieNumber, //해당 영화 id
+          likeuseraccount: currentUser, //현재 접속한 사용자의 id -> 리뷰들에 대한 좋아요를 눌렀는지 확인
+        },
+      });
+      //리뷰 정보 저장
+      setMovies(response.data);
+      setReviews(response_rv.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
-    useEffect(() => {
-      fetchData(); // fetchData 함수 호출
-  
-      if (localStorage.getItem("LoginID") != null) {
-        //localStorage 에서 "LoginID"라는 key가 있으면 로그인 된 것, 아니면 게스트 모드 -> 리뷰 작성 버튼 누를 때 로그인 화면으로 이동
-        setIsLoggedIn(true);
-      } else {
-        setIsLoggedIn(false);
-      }
-    }, [movieNumber]);
+  useEffect(() => {
+    fetchData(); // fetchData 함수 호출
 
-    console.log("Movie ID:", movieNumber);
+    if (localStorage.getItem("LoginID") != null) {
+      //localStorage 에서 "LoginID"라는 key가 있으면 로그인 된 것, 아니면 게스트 모드 -> 리뷰 작성 버튼 누를 때 로그인 화면으로 이동
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, [movieNumber]);
 
-   // 민경 - 게시일 받는 함수
-   const formatReviewDate = (dateString) => {
+  console.log("Movie ID:", movieNumber);
+
+  // 민경 - 게시일 받는 함수
+  const formatReviewDate = (dateString) => {
     const date = new Date(dateString);
     const year = date.getFullYear().toString().slice(-2); // 년도의 마지막 2자리만 추출
     const month = ("0" + (date.getMonth() + 1)).slice(-2); // 월을 2자리로 표시
@@ -125,65 +124,79 @@ const BoardList = () => {
     setEditedReview("");
   };
   const handleDelete = async (user_reviewid, user_useraccount) => {
-    console.log("리뷰 아이디: "+user_reviewid);
-    console.log("리뷰 쓴 사람"+user_useraccount);
+    console.log("리뷰 아이디: " + user_reviewid);
+    console.log("리뷰 쓴 사람" + user_useraccount);
     try {
       const response = await axios.delete(`/myapp/review`, {
         params: { reviewid: user_reviewid }, // Passing reviewid as a parameter
-        data: { // Passing useraccount and movie_id in the request body
+        data: {
+          // Passing useraccount and movie_id in the request body
           useraccount: user_useraccount,
           movie_id: movieNumber,
-        }
+        },
       });
       setReview(response.data);
       console.log("Delete response:", response.data);
-      window.location.reload(); 
+      window.location.reload();
     } catch (e) {
       console.error("Error deleting review:", e);
+    }
+  };
+  const handleEdit = async (review_content, review_rating) => {
+    try {
+      const response = await axios.put(`/myapp/review`, {
+        data: {
+          useraccount: currentUser,
+          movie_id: movieNumber,
+          content: review_content,
+          rating: review_rating,
+        },
+      });
+      setReview(response.data);
+    } catch (error) {
+      console.log("수정 오류 : "+ error);
     }
   };
 
   // 민경 - 좋아요 구현부
   // 좋아요 토글 함수
-const handleLike = async (reviewId) => {
-  try {
-    const isLiked = likesReviews.includes(reviewId);
+  const handleLike = async (reviewId) => {
+    try {
+      const isLiked = likesReviews.includes(reviewId);
 
-    // 서버로 전송할 데이터 준비
-    const requestData = {
-      useraccount: currentUser,
-      reviewid: reviewId,
-    };
+      // 서버로 전송할 데이터 준비
+      const requestData = {
+        useraccount: currentUser,
+        reviewid: reviewId,
+      };
 
-    // /reviewlike 엔드포인트로 POST 요청 보내기
-    await axios.post(`/myapp/reviewlike`, requestData);
+      // /reviewlike 엔드포인트로 POST 요청 보내기
+      await axios.post(`/myapp/reviewlike`, requestData);
 
-    // 리뷰 목록에서 해당 리뷰의 좋아요 수 변경 및 사용자의 좋아요 상태 업데이트
-    setReviews((prevReviews) =>
-      prevReviews.map((review) =>
-        review.id === reviewId
-          ? {
-              ...review,
-              likes: isLiked ? review.likes - 1 : review.likes + 1,
-              user_liked: !isLiked, // 사용자의 좋아요 상태 업데이트
-            }
-          : review
-      )
-    );
+      // 리뷰 목록에서 해당 리뷰의 좋아요 수 변경 및 사용자의 좋아요 상태 업데이트
+      setReviews((prevReviews) =>
+        prevReviews.map((review) =>
+          review.id === reviewId
+            ? {
+                ...review,
+                likes: isLiked ? review.likes - 1 : review.likes + 1,
+                user_liked: !isLiked, // 사용자의 좋아요 상태 업데이트
+              }
+            : review
+        )
+      );
 
-    // 좋아요 상태 업데이트 후에 fetchData 함수를 호출하여 해당 영역을 새로 고침
-    fetchData();
-
-    // 좋아요 상태 업데이트
-    setLikesReviews(
-      isLiked
-        ? likesReviews.filter((id) => id !== reviewId) // 좋아요 취소한 경우
-        : [...likesReviews, reviewId] // 좋아요 한 경우
-    );
-  } catch (error) {
-    console.error("Error updating like:", error);
-  }
-};
+      // 좋아요 상태 업데이트
+      setLikesReviews(
+        isLiked
+          ? likesReviews.filter((id) => id !== reviewId) // 좋아요 취소한 경우
+          : [...likesReviews, reviewId] // 좋아요 한 경우
+      );
+    } catch (error) {
+      console.error("Error updating like:", error);
+    }
+    window.location.reload();
+  };
 
   //=============handleLike ================
 
@@ -270,7 +283,9 @@ const handleLike = async (reviewId) => {
                 <br />
                 <div>
                   <button
-                    className={`likes_button ${review.user_liked ? "liked" : ""}`}
+                    className={`likes_button ${
+                      review.user_liked ? "liked" : ""
+                    }`}
                     onClick={() => handleLike(review.reviewid)}
                   >
                     {review.user_liked === false ? "♡" : "♥"}
@@ -283,10 +298,26 @@ const handleLike = async (reviewId) => {
                 {/* 민경 - 수정 버튼을 누르면 저장 버튼으로 변경하는 함수 필요한지 확인 */}
                 {isLoggedIn && review.useraccount === currentUser && (
                   <>
-                    <button className="edit_button">수정</button>
+                    <button
+                      className="edit_button"
+                      onClick={() => handleEdit(review.content, review.rating)}
+                    >
+                      수정
+                    </button>
                     <button className="save_button">저장</button>
-                    <button className="cancel_button"  onClick={handleCancelEdit}  >  취소</button>
-                    <button  className="delete_button"  onClick={() =>  handleDelete(  review.reviewid,  review.useraccount  )  }  >
+                    <button
+                      className="cancel_button"
+                      onClick={handleCancelEdit}
+                    >
+                      {" "}
+                      취소
+                    </button>
+                    <button
+                      className="delete_button"
+                      onClick={() =>
+                        handleDelete(review.reviewid, review.useraccount)
+                      }
+                    >
                       삭제
                     </button>
                   </>
