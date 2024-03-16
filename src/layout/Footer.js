b1fbf74695b3502677bac8b55fd9d35a3ae93b3a
GoMyamMing <mkkwak95@gmail.com>
2024-03-16 19:10:07 +0900

    Footer 공유

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,15 +1,33 @@
 /* Footer.js */
 import React from 'react';
+import styles from "./Footer.Module.css";
 
 const Footer = () => {
   return (
-    <><div id='wrapper'>
-      <div>
-        footer
-        <br/>
-        멀티잇 풀스텍 부트캠프 6조 
-      </div>
-    </div><footer></footer></>
+    <>
+      <footer>
+        <div id='wrapper'>
+          <div>
+            <h4> MULTICAMPUS 19TH SEMI PROJECT @TEAM_6 (REVIEWRARY) </h4>
+            <br></br>
+          </div>
+          <div>
+            <h3>FRONT</h3>
+            <p>곽민경 <br></br> <a href="mailto:mkkwak95@gmail.com">mkkwak95@gmail.com</a></p>
+            <p>유하영 <br></br> <a href="mailto:yoohayung10309@gmail.com">yoohayung10309@gmail.com</a></p>
+          </div>
+          <br></br>
+          <div>
+            <h3>BACK</h3>
+            <p>유욱현 <br></br> <a href="mailto:why970126@gmail.com">why970126@gmail.com</a></p> 
+            <p>전민훈 <br></br> <a href="mailto:a01066215336@gmail.com">a01066215336@gmail.com</a></p>
+          </div>
+        </div>
+        <div className="copyright">
+          <p>ⓒ 2024 TEAM_6 (REVIEWRARY) All rights reserved.</p>
+        </div>
+      </footer>
+    </>
   );
 };
 
