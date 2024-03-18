92255c1c23eed7e976ee47fd31898f49a2bd6680
hayung <yoohayung10309@gmail.com>
2024-03-18 09:52:08 +0900

    boardList css fix

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -12,13 +12,13 @@ const Footer = () => {
             <br></br>
           </div>
           <div>
-            <h3>FRONT</h3>
+            <h3 className='each_role'>FRONT</h3>
             <p>곽민경 <br></br> <a href="mailto:mkkwak95@gmail.com">mkkwak95@gmail.com</a></p>
             <p>유하영 <br></br> <a href="mailto:yoohayung10309@gmail.com">yoohayung10309@gmail.com</a></p>
           </div>
           <br></br>
           <div>
-            <h3>BACK</h3>
+            <h3 className='each_role'>BACK</h3>
             <p>유욱현 <br></br> <a href="mailto:why970126@gmail.com">why970126@gmail.com</a></p> 
             <p>전민훈 <br></br> <a href="mailto:a01066215336@gmail.com">a01066215336@gmail.com</a></p>
           </div>
